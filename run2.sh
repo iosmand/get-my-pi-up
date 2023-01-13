d5e75e1c4a18868554794169d3ce6309ca87640f
Osman Dağdeviren <my@iosman.us>
2023-01-13 20:25:08 +0300

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,22 +1,3 @@
-#mkdir ~/setup
-#cd ~/setup
-sudo apt install -y uidmap apparmor jq wget curl udisks2 libglib2.0-bin network-manager dbus lsb-release systemd-journal-remote
-##get-docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-DRY_RUN=1
-sudo sh ./get-docker.sh
-dockerd-rootless-setuptool.sh install
-
-
-##portainer
-#https://pimylifeup.com/raspberry-pi-portainer/
-sudo docker run -d -p 9000:9000 -p 9443:9443 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-
-##install hass
-sudo dpkg -i ./hass/os-agent_1.4.1_linux_aarch64.deb
-sudo dpkg -i ./hass/supervised-installer/homeassistant-supervised.deb
-
 ##vnc önce lubuntu
 sudo apt install --install-recommends --install-suggests lubuntu-desktop -y
 sudo apt install libraspberrypi0 -y
