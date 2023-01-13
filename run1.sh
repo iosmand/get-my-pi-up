a46d29c5c41539fe478ab9c5f3de585973ad037f
Osman Dağdeviren <my@iosman.us>
2023-01-13 19:50:57 +0300

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -39,3 +39,23 @@ cat /proc/sys/vm/vfs_cache_pressure
 #https://waldorf.waveform.org.uk/2022/making-jammy-less-dodgy.html
 #sudo sed -i -e 's/$/ multipath=off/' /boot/firmware/cmdline.txt
 #sudo reboot
+
+#mkdir ~/setup
+#cd ~/setup
+sudo apt install -y uidmap apparmor jq wget curl udisks2 libglib2.0-bin network-manager dbus lsb-release systemd-journal-remote
+##get-docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+DRY_RUN=1
+sudo sh ./get-docker.sh
+dockerd-rootless-setuptool.sh install
+
+
+##portainer
+#https://pimylifeup.com/raspberry-pi-portainer/
+sudo docker run -d -p 9000:9000 -p 9443:9443 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+
+##install hass
+sudo dpkg -i ./hass/os-agent_1.4.1_linux_aarch64.deb
+sudo dpkg -i ./hass/supervised-installer/homeassistant-supervised.deb
+sudo reboot
