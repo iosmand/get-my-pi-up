9de109d3a81b026fa01eec958302b6228406c8ab
Osman Dağdeviren <my@iosman.us>
2023-01-14 10:24:10 +0300

    Update 1-get-docker-hass.sh

--- 1-get-docker-hass.sh
+++ 1-get-docker-hass.sh
@@ -9,7 +9,7 @@ dockerd-rootless-setuptool.sh install
 
 ##portainer
 #https://pimylifeup.com/raspberry-pi-portainer/
-sudo docker run -d -p 9000:9000 -p 9443:9443 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#sudo docker run -d -p 9000:9000 -p 9443:9443 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 
 ##install hass
